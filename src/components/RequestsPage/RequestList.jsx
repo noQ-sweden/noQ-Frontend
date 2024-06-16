4f14cb0e5c36c5d77bf1f55e3cb1f7d14df1c97a
Jani Rutanen <jani@noq.nu>
2024-06-16 13:55:35 +0200

    Add funtionality to accept and decline bookings

--- src/components/RequestsPage/RequestList.jsx
+++ src/components/RequestsPage/RequestList.jsx
@@ -1,58 +1,268 @@
 import React, { useState, useEffect } from 'react';
-import { BsChatRightText } from "react-icons/bs";
+import axios from './../../api/AxiosNoqApi';
 
-export default function RequestList({ setShowPopup, setShowTable, requests: propRequests }) { 
+
+export default function RequestList() { 
 
     const [requests, setRequests] = useState([]);
+    const [undoRequests, setUndoRequests] = useState([]);
 
-    const handleAssignOnClick = (id) => {
-        setShowPopup(true);
-        setShowTable(false);
-        console.log(id);
+    const getDayNumber = (date) => {
+        const values = date.split('-');
+        return values[2];
     }
 
-    const handleRejectOnClick = (id) => {
-        // Filter out the request with the given id
-        const updatedRequests = requests.filter(request => request.id !== id);
-        // Update the state with the filtered requests
-        setRequests(updatedRequests);
-        console.log(id);
-    }
-
-    // Update the requests state when propRequests change
-    useEffect(() => {
-        if (Array.isArray(propRequests)) {
-            setRequests(propRequests);
+    const getMonth = (date) => {
+        const values = date.split('-');
+        var month = '';
+        switch(values[1]) {
+            case '01':
+                month="JAN";
+                break;
+            case '02':
+                month="FEB";
+                break;
+            case '03':
+                month="MAR";
+                break;
+            case '04':
+                month="APR";
+                break;
+            case '05':
+                month="MAJ";
+                break;
+            case '06':
+                month="JUN";
+                break;
+            case '07':
+                month="JUL";
+                break;
+            case '08':
+                month="AUG";
+                break;
+            case '09':
+                month="SEP";
+                break;
+            case '10':
+                month="OCT";
+                break;
+            case '11':
+                month="NOV";
+                break;
+            default:
+                month="DEC";
         }
-    }, [propRequests]);
+        return month;
+    }
+
+    const getGender = (gender) => {
+        if (gender === 'K') {
+            return "Kvinna";
+        } else if (gender === 'M') {
+            return "Man";
+        } else {
+            return "Annan"
+        }
+    }
+
+    const getStatus = (status) => {
+        if (status === 'accepted') {
+            return "Tilldelat";
+        } else if (status === 'rejected') {
+            return "Nekad";
+        } else {
+            return ""
+        }
+    }
+
+    const handleAssignOnClick = (bookingId) => {
+        const url = "api/host/pending/" + bookingId + "/appoint";
+        axios.patch (url)
+            .then (function (response) {
+                console.log(response);
+                if (response.status === 200 && response?.data !== "") {
+                    undoRequests.push(response?.data);
+                    setUndoRequests(undoRequests);
+                    fetchPendingRequests();
+                }
+            })
+            .catch((error) => {
+                console.log("Error while updating booking status to accepted.", error);
+            });
+    }
+
+    const handleRejectOnClick = (bookingId) => {
+        const url = "api/host/pending/" + bookingId + "/decline";
+        axios.patch (url)
+            .then (function (response) {
+                console.log(response);
+                if (response.status === 200 && response?.data !== "") {
+                    undoRequests.push(response?.data);
+                    setUndoRequests(undoRequests);
+                    fetchPendingRequests();
+                }
+            })
+            .catch((error) => {
+                console.log("Error while updating booking status to accepted.", error);
+            });
+    }
+
+    const handleUndoOnClick = (bookingId) => {
+        const url = "api/host/bookings/" + bookingId + "/setpending";
+        axios.patch (url)
+            .then (function (response) {
+                console.log(response);
+                if (response.status === 200 && response?.data !== "") {
+                    const idx = undoRequests.findIndex(obj => obj.id === parseInt(bookingId));
+                    if (idx > -1) {
+                        undoRequests.splice(idx, 1);
+                    }
+                    setUndoRequests(undoRequests);
+                    fetchPendingRequests();
+                }
+            })
+            .catch((error) => {
+                console.log("Error while updating booking status to accepted.", error);
+            });
+    }
+
+    const fetchPendingRequests = async () => {
+        axios.get ('api/host/pending')
+            .then (function (response) {
+                if (response.status === 200) {
+                    setRequests(response?.data);
+                }
+            })
+            .catch((error) => {
+                console.log("Error while fetching pending bookings.", error);
+            });
+    }
+
+    useEffect(() => {
+        fetchPendingRequests();
+    }, []);
 
     return (
-        <div className="grid grid-cols-1 gap-4">
-            <div className='grid grid-cols-9 gap-2 text-center'>
-                <h3 className='text-xl'>Välj Flera</h3>
-                <h3 className='text-xl'>Värd</h3>
-                <h3 className='text-xl'>Meddelande</h3>
-                <h3 className='text-xl'>Mottagen</h3>
-                <h3 className='text-xl'>Rumstyp</h3>
-                <h3 className='text-xl'>Incheckning</h3>
-                <h3 className='text-xl'>Utcheckning</h3>
-                <h3 className='text-xl'></h3>
-                <h3 className='text-xl'></h3>
-            </div>
-            {requests.map(request => (
-                <div key={request.id} className='grid grid-cols-9 gap-2 items-center text-center'>
-                    <input type="checkbox" />
-                    <div>{request.host}</div>
-                    <div className='flex justify-center items-center mt-3'><BsChatRightText size={20} /></div>
-                    <div>{request.startDate}</div>
-                    <div>{request.roomType}</div>
-                    <div>{request.startDate}</div>
-                    <div>{request.endDate}</div>
-                    <div><button className="bg-green-200 text-Request-ButtonText-Green hover:bg-green-700 font-bold rounded border-solid border-2 border-Request-ButtonBorder-Green p-2 pr-4 pl-4 hover:text-white" onClick={() => handleAssignOnClick(request.id)}>Tilldela</button></div>
-                    <div><button className="bg-red-200 text-Request-ButtonText-red hover:bg-red-700 font-bold rounded border-solid border-2 border-Request-ButtonBorder-red p-2 pr-4 pl-4 hover:text-white" onClick={() => handleRejectOnClick(request.id)}>Neka</button></div>
+        <div className="grid grid-cols-1 gap-2">
+            <h2 className='text-2xl mb-4'>Förfrågningar</h2>
+            { requests.map(request => (
+                <div key={request.id} className='
+                    grid
+                    grid-cols-[10px_150px_2fr_2fr_100px]
+                    rounded
+                    border-2
+                    border-800-green
+                    p-2
+                    '>
+                    <div className='grid grid-rows-1 items-center text-center'>
+                        <input className='size-4' type="checkbox" />
+                    </div>
+                    <div className='grid grid-rows-1 items-center text-center'>
+                        <div className='leading-3'>
+                            <p className='font-bold text-2xl'>{getDayNumber(request.start_date)}</p>
+                            <p className='text-lg'>{getMonth(request.start_date)}</p>
+                        </div>
+                    </div>
+                    <div className='grid grid-rows-1 gap-1 items-center text-left'>
+                        <div><b>Värd:</b> {request.product.host.name}</div>
+                        <div><b>Rumstyp:</b> {request.product.type}</div>
+                    </div>
+                    <div className='grid grid-rows-1 gap-1 items-center text-left'>
+                        <div><b>Unokod:</b> {request.user.unokod}</div>
+                        <div><b>Kön:</b> {getGender(request.user.gender)}</div>
+                    </div>
+                    <div className='grid grid-rows-2 gap-2 items-right'>
+                        <div>
+                            <button className="
+                                bg-green-600
+                                hover:bg-green-700
+                                text-white
+                                font-semibold
+                                text-m
+                                align-middle
+                                w-20
+                                h-7
+                                rounded
+                                focus:outline-none
+                                focus:shadow-outline"
+                                onClick={() => handleAssignOnClick(request.id)}>
+                                Tilldela
+                            </button>
+                        </div>
+                        <div>
+                            <button className="
+                                bg-gray-200
+                                hover:bg-gray-300
+                                border-slate-800
+                                text-gray-500
+                                font-semibold
+                                text-m
+                                align-middle
+                                w-20
+                                h-7
+                                rounded
+                                focus:outline-none
+                                focus:shadow-outline"
+                                onClick={() => handleRejectOnClick(request.id)}>
+                                Neka
+                            </button>
+                        </div>
+                    </div>
                 </div>
             ))}
+            { undoRequests.length > 0 &&
+                <h2 className='text-2xl mb-4 mt-8'>Senaste ändringar</h2>
+            }
+            { undoRequests.map(undoRequest => (
+                <div key={undoRequest.id} className='
+                    grid
+                    grid-cols-[70px_150px_2fr_2fr_100px]
+                    rounded
+                    border-2
+                    border-800-green
+                    p-2
+                    '>
+                    <div className='grid grid-rows-1 items-center text-left font-bold text-xl text-green-noQ'>
+                        { getStatus(undoRequest.status.description) }
+                    </div>
+                    <div className='grid grid-rows-1 items-center text-center'>
+                        <div className='leading-3'>
+                            <p className='font-bold text-2xl'>{ getDayNumber(undoRequest.start_date) }</p>
+                            <p className='text-lg'>{ getMonth(undoRequest.start_date) }</p>
+                        </div>
+                    </div>
+                    <div className='grid grid-rows-1 gap-1 items-center text-left'>
+                        <div><b>Värd:</b> { undoRequest.product.host.name }</div>
+                        <div><b>Rumstyp:</b> { undoRequest.product.type }</div>
+                    </div>
+                    <div className='grid grid-rows-1 gap-1 items-center text-left'>
+                        <div><b>Unokod:</b> { undoRequest.user.unokod }</div>
+                        <div><b>Kön:</b> { getGender(undoRequest.user.gender) }</div>
+                    </div>
+                    <div className='grid grid-rows-1 gap-2 items-right'>
+                        <div>
+                            <button className="
+                                bg-green-600
+                                hover:bg-green-700
+                                text-white
+                                font-semibold
+                                text-m
+                                align-middle
+                                w-20
+                                h-7
+                                rounded
+                                focus:outline-none
+                                focus:shadow-outline"
+                                onClick={() => handleUndoOnClick(undoRequest.id)}>
+                                Ångra
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            ))}
+
         </div>
+        
     );
 }
 
