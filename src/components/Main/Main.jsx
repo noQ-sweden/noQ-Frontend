f743a1c8add74d91605ade2df02c592af3d78459
Jani Rutanen <jani@noq.nu>
2024-06-23 12:20:21 +0200

    Changes font and colors to match the design doc

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,6 @@ import React from "react";
 export default function Main({ children }) {
   return (
     /* Main, Här ska allt skit som dyka upp dyka upp */
-    <div className="flex justify-center bg-grey-noQ">{children}</div>
+    <div className="flex justify-center bg-background-white">{children}</div>
   );
 }
