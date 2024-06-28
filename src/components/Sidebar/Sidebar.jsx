cbd8e8340e110521d70e291a7ea273dddf09e884
Selsabeel A <90451756+SelsabeelA@users.noreply.github.com>
2024-06-28 11:48:26 +0300

    uppdatera sidomenyn, ticket 456

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,22 +1,24 @@
 import React, { useState } from "react";
 import {
-  FaCog,
   FaChartPie,
+  FaCog,
   FaCalendarAlt,
   FaReceipt,
   FaUser,
   FaBell,
+  FaUserAlt,
+  FaSignOutAlt,
 } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { GiHamburgerMenu, GiCancel } from "react-icons/gi";
-import noQicon from "./../../assets/images/noQiconWhite.svg"
+import noQicon from "./../../assets/images/noQiconNoQGreen.svg";
 
 export default function Sidebar() {
   const navigate = useNavigate();
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   const liStyle =
-    "py-5 border-green-noQ hover:bg-gray-300 hover:text-green-noQ transition-colors duration-200";
+    "py-5 text-gray-500 border-green-noQ hover:bg-green-noQ hover:text-white transition-colors duration-200 hover:rounded-full";
   const liTextStyle = "flex gap-4 pl-10";
   const liTextStyleClosed = "flex gap-4 px-5";
 
@@ -24,26 +26,41 @@ export default function Sidebar() {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
+  const handleLogout = () => {
+    // Clear user session data here (localStorage, cookies, etc.)
+    localStorage.clear(); // Example: clearing all localStorage items
+
+    // Redirect to the starting page
+    navigate("/", { replace: true });
+    // Reload the page to reset the application state
+    window.location.reload();
+  };
+
   const sidebarItems = [
     { icon: FaChartPie, label: "Överblick", sideBarLink: "admin" },
-    { icon: FaCalendarAlt, label: "Kalender" },
-    { icon: FaBell, label: "Förfrågan", sideBarLink: "host/requests" },
-    { icon: FaUser, label: "Gäster" },
+    { icon: FaBell, label: "Förfrågningar", sideBarLink: "host/requests" },
     { icon: FaReceipt, label: "Fakturering" },
     { icon: FaCog, label: "Inställningar" },
+    { icon: FaUserAlt, label: "Användare" },
+    { icon: FaSignOutAlt, label: "Logga ut", action: handleLogout },
   ];
 
   return (
     <div
-      className={`flex flex-col text-white min-h-screen bg-green-noQ m-0 select-none ${
+      className={`flex flex-col text-white min-h-screen bg-gray-100 m-0 select-none ${
         !isSidebarOpen ? "w-25" : "w-64"
-      }`}>
-      <ul className={`bg-green-noQ text-xl ${isSidebarOpen ? "" : "w-full"}`}>
-        <div className="flex justify-between flex-row items-start my-6 px-5 pr-3">
-          <a className={` ${isSidebarOpen ? "pl-5 w-24" : "hidden"}`} href="/">
-            <img src={noQicon} alt="noQ Logo" />
+      }`}
+    >
+      <ul className={`bg-green-noq text-xl ${isSidebarOpen ? "" : "w-full"}`}>
+        <div className="flex flex-col items-center my-6">
+          <a className={` ${isSidebarOpen ? "w-full" : "hidden"}`} href="/">
+            <img
+              src={noQicon}
+              alt="noQ Logo"
+              className={`h-20 mx-auto ${isSidebarOpen ? "w-auto" : "w-16"}`}
+            />
           </a>
-          <button onClick={toggleSidebar}>
+          <button onClick={toggleSidebar} className="mt-4">
             {isSidebarOpen ? (
               <GiCancel size="25" />
             ) : (
@@ -52,11 +69,21 @@ export default function Sidebar() {
           </button>
         </div>
         <div>
-          {sidebarItems.map(({ icon: Icon, label, sideBarLink }) => (
-            <div onClick={() => navigate(sideBarLink, { replace: false })} key={label}>
-              <li className={liStyle} >
+          {sidebarItems.map(({ icon: Icon, label, sideBarLink, action }) => (
+            <div
+              onClick={() => {
+                if (action) {
+                  action();
+                } else if (sideBarLink) {
+                  navigate(sideBarLink, { replace: false });
+                }
+              }}
+              key={label}
+            >
+              <li className={liStyle}>
                 <span
-                  className={isSidebarOpen ? liTextStyle : liTextStyleClosed}>
+                  className={isSidebarOpen ? liTextStyle : liTextStyleClosed}
+                >
                   <Icon size="25" />
                   {isSidebarOpen && label}
                 </span>
