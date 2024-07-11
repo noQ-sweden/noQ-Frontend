ba72a710461d7022b9546756c36139f2d5a04dbb
Ilona Kamash <ilonka.kamash@gmail.com>
2024-07-11 14:46:12 +0200

    add file with Flika info and ArrowButton

--- src/components/RoomPage/tailwind.config.js
+++ src/components/RoomPage/tailwind.config.js
@@ -4,13 +4,19 @@ module.exports = {
         padding: {
             '23': '23px',  
             '22': '22px',  
+            '17':'17px'
           },
         width: {
             '970': '970px',  
           },
         height: {
           '70': '70px',
+        },
+        margin:{
+          '39':'39px',
+          '22':'22px'
         }
+
 
       }
     }
