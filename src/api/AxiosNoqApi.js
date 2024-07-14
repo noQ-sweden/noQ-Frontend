04755cec21cc1e34058db7d519e2ef2572fd6e53
Jani Rutanen <jani@noq.nu>
2024-07-14 17:20:25 +0200

    Fixes issue with not sending csrftoken

--- src/api/AxiosNoqApi.js
+++ src/api/AxiosNoqApi.js
@@ -2,24 +2,23 @@ import axios from 'axios';
 import { axiosMockNoqApi } from './mockApi/mockApi';
 import Cookies from 'js-cookie';
 
-
-
-const csrftoken = Cookies.get('csrftoken');
-
-axios.interceptors.request.use(config => {
-    if (config.method === 'post' || config.method === 'put') {
-        config.headers['X-CSRFToken'] = csrftoken;
-    }
-    return config;
-});
-
 const axiosNoqApi = axios.create({
     baseURL: "http://localhost:8000",
     headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json",
+
       },
       withCredentials: true,
 });
 
+axiosNoqApi.interceptors.request.use(config => {
+    const csrftoken = Cookies.get('csrftoken');
+
+    if (config.method === 'post' || config.method === 'put') {
+        config.headers['X-CSRFToken'] = csrftoken;
+    }
+
+    return config;
+});
 
 export default import.meta.env.NOQ_IS_MOCK_API === 'true' ? axiosMockNoqApi : axiosNoqApi;
