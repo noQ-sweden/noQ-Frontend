8c4c601dd0eb6a6f3830a912b68fb31687cfad96
jrutanen <rautasissi@gmail.com>
2024-07-15 09:31:58 +0200

    Adds Dockerfile and changes build Vite config for build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/",
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
   plugins: [react()],
   envPrefix: 'NOQ',
   define:{
