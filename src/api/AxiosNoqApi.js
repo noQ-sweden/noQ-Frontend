710af7d4ab25a80768cf14888b4ce0c77b7b148e
Tobias Johansson <tobiasp91@hotmail.com>
2024-07-30 18:28:23 +0200

    test för att kunna köra i prod eller dev miljö genom att använda dot-cli -> se i package.json

--- src/api/AxiosNoqApi.js
+++ src/api/AxiosNoqApi.js
@@ -1,12 +1,18 @@
-import axios from 'axios';
-import { axiosMockNoqApi } from './mockApi/mockApi';
+console.log("NOQ ENV:", import.meta.env.NOQ_ENVIRONMENT);
+console.log("is MOCK", import.meta.env.NOQ_IS_MOCK_API);
+console.log("base URL", import.meta.env.NOQ_BASE_URL);
+
+import axios from "axios";
+import { axiosMockNoqApi } from "./mockApi/mockApi";
 
 const axiosNoqApi = axios.create({
-    baseURL: "http://51.21.97.243", //"http://localhost",
-    headers: {
-        "Content-type": "application/json",
-      },
-      withCredentials: true,
+  baseURL: import.meta.env.NOQ_BASE_URL,
+  headers: {
+    "Content-type": "application/json",
+  },
+  withCredentials: true,
 });
 
-export default import.meta.env.NOQ_IS_MOCK_API === 'true' ? axiosMockNoqApi : axiosNoqApi;
+export default import.meta.env.NOQ_IS_MOCK_API === "true"
+  ? axiosMockNoqApi
+  : axiosNoqApi;
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  envPrefix: 'NOQ',
-  define:{
-    'process.env.NOQ_IS_MOCK_API':JSON.stringify(process.env.NOQ_IS_MOCK_API)
-  }
+export default defineConfig(({ mode }) => {
+  console.log(`Current mode: ${mode}`);
+
+  return {
+    plugins: [react()],
+    envPrefix: "NOQ_",
+    define: {
+      "process.env.NOQ_IS_MOCK_API": JSON.stringify(
+        process.env.NOQ_IS_MOCK_API
+      ),
+    },
+  };
 });
