7cd44f3d5c9273a837c95fe877366e28d691afd3
Abubaker <Abubakersalah4@gmail.com>
2024-09-15 11:36:36 +0200

    Checka in brukare knappen.

--- src/components/Admin/IncomingGuests.jsx
+++ src/components/Admin/IncomingGuests.jsx
@@ -3,6 +3,9 @@ import axios from './../../api/AxiosNoqApi';
 import Panel from "../Common/Panel";
 
 
+
+
+
 export default function IncomingGuests() {
 
     const [incomingBookings, setIncomingBookings] = useState([])
@@ -10,17 +13,38 @@ export default function IncomingGuests() {
     useEffect( () => {
         axios.get ('/api/host/bookings/incoming')
         .then ( (response) => {
+            console.log(response)
+            
             if (response.status === 200) {
                 setIncomingBookings(response?.data)
             }
+            
         })
         .catch((error) => {
             console.log("Error while fetching incoming bookings data.", error);
         });
     }, []);
 
-    const handleCheckIn = () => {
+    const handleCheckIn = (bookingId) => {
         //TODO
+        axios.get(`/api/host/bookings/${bookingId}/checkin`)
+        .then((response )=> {
+            
+            if(response.status === 200) {
+                setIncomingBookings(prevBokings => 
+                    prevBokings.map(booking => 
+                        booking.id === bookingId
+                        ? {...booking, status: {...booking.status, description: 'Checked In'}} :booking
+                    )
+        
+                )
+            }
+
+        }) 
+        .catch((error) => {
+            console.log("Error while checking in.")
+        })
+        
     }
 
     return (
@@ -40,21 +64,24 @@ export default function IncomingGuests() {
                                     <td className='tracking-tight'>{booking.user.first_name} {booking.user.last_name}</td>
                                     <td className='tracking-tight'>{booking.status.description}</td>
                                     <td className='p-2 tracking-tight text-right'>
-                                        <button className="
-                                            bg-green-600
-                                            hover:bg-green-700
-                                            text-white
-                                            font-semibold
-                                            text-m
-                                            align-middle
-                                            w-32
-                                            h-7
-                                            rounded
-                                            focus:outline-none
-                                            focus:shadow-outline"
-                                            onClick={() => handleCheckIn()}>
-                                            Incheckning
-                                        </button>
+                                        {booking.status.description !== 'Checked In' && (
+                                             <button className="
+                                             bg-green-600
+                                             hover:bg-green-700
+                                             text-white
+                                             font-semibold
+                                             text-m
+                                             align-middle
+                                             w-32
+                                             h-7
+                                             rounded
+                                             focus:outline-none
+                                             focus:shadow-outline"
+                                             onClick={() => handleCheckIn(booking.id)}>
+                                             Incheckning
+                                         </button>
+                                        )}
+                                       
                                     </td>
                                 </tr>
                             ))}
