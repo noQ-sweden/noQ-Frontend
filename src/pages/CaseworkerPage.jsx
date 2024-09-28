ab9a592ec82b3b53ef33cbec7cd2251f060278d9
xelleze <abdunashef@gmail.com>
2024-09-28 22:26:48 +0200

    cleaned up a bit

--- src/pages/CaseworkerPage.jsx
+++ src/pages/CaseworkerPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from './../api/AxiosNoqApi'; // Adjust the path as needed
+import axios from './../api/AxiosNoqApi'; 
 import ArrowUpIcon from '../assets/images/arrowUpIcon.svg';
 import ArrowDownIcon from '../assets/images/arrowDownIcon.svg';
 
@@ -7,7 +7,7 @@ export default function CaseworkerPage() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [expandedHosts, setExpandedHosts] = useState({}); // State to track expanded cards
+  const [expandedHosts, setExpandedHosts] = useState({});
 
   // Variable for text
   const descriptionTitelText = " - Rum";
@@ -22,7 +22,6 @@ export default function CaseworkerPage() {
         const fetchedData = response.data;
         setData(fetchedData);
 
-        // Automatically expand the first host and collapse others
         const firstHostName = fetchedData.length > 0 ? fetchedData[0].host.name : null;
         const initialExpandedHosts = fetchedData.reduce((acc, item) => {
           acc[item.host.name] = item.host.name === firstHostName;
@@ -50,8 +49,6 @@ export default function CaseworkerPage() {
       return acc;
     }, {});
   };
-
-
 
   // Calculate available and total rooms for a host
   const calculateRoomAvailability = (rooms) => {
@@ -85,17 +82,15 @@ export default function CaseworkerPage() {
   return (
     <div className="w-full p-4">
 
-      {/* Loop through grouped data */}
       {Object.keys(groupedData).map((hostName, index) => {
         const rooms = groupedData[hostName];
-        const totalRooms = rooms.length; // Count the number of rooms for this host
-        const isExpanded = expandedHosts[hostName]; // Check if the current host is expanded
+        const totalRooms = rooms.length; 
+        const isExpanded = expandedHosts[hostName]; 
         
         const roomAvailability = calculateRoomAvailability(rooms);
 
         return (
           <div key={hostName} className="bg-white flex flex-col rounded-2xl border-2 border-grey-100 shadow my-4 p-3">
-            {/* Host Information (Always Visible) */}
             <div className="flex justify-between items-center">
               <h2 className="text-2xl font-bold">{hostName}{descriptionTitelText}</h2>
               <span className="font-bold">
@@ -103,10 +98,7 @@ export default function CaseworkerPage() {
               </span>
             </div>
 
-            {/* Conditionally render a separator when the card is closed */}
             {!isExpanded && <div className="border-b mb-1 p-2"></div>}
-
-            {/* Toggle Details */}
             {isExpanded && (
               <>
                 <p className="text-gray-500">
