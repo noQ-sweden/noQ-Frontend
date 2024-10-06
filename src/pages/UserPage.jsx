611492b112013b068341fbb0bf0f7ed51c7292c1
Mario Fernández <nilovinilo1@gmail.com>
2024-10-06 23:21:24 +0200

    Completed SEO, updating user tab  - Fixes #48

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,14 +1,16 @@
 import AccommodationList from "./../components/User/AccommodationList";
 import AccommodationPanel from "../components/User/AccommodationPanel";
+import SEO from "../components/SEO";
 
 export default function UserPage() {
-    return (
-        <>
-            <div className="mr-8 sm:mr-3" id="UserPage">
-                <AccommodationPanel title="Välj boställe">
-                        <AccommodationList />
-                </AccommodationPanel>
-            </div>
-        </>
+  return (
+    <>
+      <SEO title={`Användare | NoQ - Trygg Plats för att alla förtjänar det`} />
+      <div className="mr-8 sm:mr-3" id="UserPage">
+        <AccommodationPanel title="Välj boställe">
+          <AccommodationList />
+        </AccommodationPanel>
+      </div>
+    </>
   );
 }
