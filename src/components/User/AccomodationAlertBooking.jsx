2440560b77a0188f0401417fd6b2b194f4732c21
Jani Rutanen <jani@noq.nu>
2024-10-07 18:38:16 +0200

    Updates button color for booking confirmation and changes order for month-date in weekly view.

--- src/components/User/AccomodationAlertBooking.jsx
+++ src/components/User/AccomodationAlertBooking.jsx
@@ -22,7 +22,7 @@ export default function AccommodationAAlertBooking({success, closeAlert}) {
                     </>
                 )}
                 <button
-                    className="mt-6 bg-blue-500 text-white px-4 py-2 rounded" 
+                    className="mt-6 bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded"
                     onClick={closeAlert}
                 >
                     Stäng
