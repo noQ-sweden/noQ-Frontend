c36ad09cdd08bf2da07d086f2320fcd3b493fc39
Abubaker <Abubakersalah4@gmail.com>
2024-11-21 04:06:40 +0100

    Update CaseworkerStatisticsPage.jsx

--- src/pages/CaseworkerStatisticsPage.jsx
+++ src/pages/CaseworkerStatisticsPage.jsx
@@ -9,6 +9,7 @@ const CaseworkerStatisticsPage = () => {
         axios.get (url)
         .then ((response) => {
             if (response.status === 200) {
+                console.log('');
             } else {
                 console.log('Error while fetching overview data.');
             }
