732c29c43f99f9bdd3e1459f4f2e390b6aaa552d
Mario Fernández <nilovinilo1@gmail.com>
2024-11-28 20:20:31 +0100

    Refactor PasswordConfirmationPopup and UserManagementPage: - Updated PasswordConfirmationPopup:   - Refactored form structure to use onSubmit for proper form submission.   - Improved button types: Bekräfta now uses type=submit and Avbryt uses type=button.   - Enhanced password mismatch error handling and reset states on success or closure. - Updated UserManagementPage:   - Optimized popup handling with openPopup and closePopup functions.   - Integrated improved PasswordConfirmationPopup component.   - Streamlined delete user and password reset workflows.   - Removed redundant state variables for cleaner code. - Enhanced UX with better error messages, validations, and simplified workflows.

--- src/components/PasswordConfirmationPopup.jsx
+++ src/components/PasswordConfirmationPopup.jsx
@@ -17,13 +17,19 @@ const PasswordConfirmationPopup = ({ isOpen, onClose, onConfirm }) => {
     setPasswordMatchError(false);
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
     if (password !== confirmPassword) {
       setPasswordMatchError(true);
       return;
     }
 
     onConfirm(password);
+    setPassword("");
+    setConfirmPassword("");
+    setPasswordMatchError(null);
+    onClose();
   };
 
   if (!isOpen) return null;
@@ -31,46 +37,47 @@ const PasswordConfirmationPopup = ({ isOpen, onClose, onConfirm }) => {
   return (
     <div className="fixed inset-0 items-center justify-center z-50 bg-gray-800 bg-opacity-50">
       <div className="bg-white rounded-lg shadow-lg p-6 w-96">
-        <h2 className="text-lg font-bold mb-4">Ändra Löseord</h2>
-        <input
-          type="password"
-          name="password"
-          autoComplete="off"
-          value={password}
-          onChange={handleChange}
-          placeholder="Skriv in det nya löseordet"
-          // required={!isEditing}
-          className="border rounded border-gray-400 py-1 px-3 w-full"
-        />
+        <h2 className="text-lg font-bold mb-4">Nytt Löseord</h2>
 
-        <input
-          type="password"
-          name="confirmPassword"
-          autoComplete="off"
-          value={confirmPassword}
-          onChange={handleConfirmPasswordChange}
-          placeholder="Bekräfta lösenord"
-          // required={!isEditing}
-          className="border rounded border-gray-400 py-1 px-3 w-full"
-        />
-        {passwordMatchError && (
-          <p className="text-red-500">Lösenorden matchar inte.</p>
-        )}
+        <form type="submit">
+          <input
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="off"
+            value={password}
+            onChange={handleChange}
+            placeholder="Skriv in det nya löseordet"
+            className="border rounded border-gray-400 py-1 px-3 w-full mb-3"
+          />
 
-        <div className="flex justify-end space-x-4">
-          <button
-            className="bg-gray-200 text-gray-600 px-4 py-2 rounded"
-            onClick={onClose}
-          >
-            Avbryt
-          </button>
-          <button
-            className="bg-red-500 text-white px-4 py-2 rounded"
-            onClick={handleSubmit}
-          >
-            Bekräfta
-          </button>
-        </div>
+          <input
+            type="password"
+            name="confirmPassword"
+            autoComplete="off"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+            placeholder="Bekräfta lösenord"
+            className="border rounded border-gray-400 py-1 px-3 w-full mb-3"
+          />
+          {passwordMatchError && (
+            <p className="text-red-500">Lösenorden matchar inte.</p>
+          )}
+          <div className="flex justify-end space-x-4">
+            <button
+              className="bg-gray-200 text-gray-600 px-4 py-2 rounded"
+              onClick={onClose}
+            >
+              Avbryt
+            </button>
+            <button
+              className="bg-red-500 text-white px-4 py-2 rounded"
+              onClick={handleSubmit}
+            >
+              Bekräfta
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/pages/UserManagementPage.jsx
+++ src/pages/UserManagementPage.jsx
@@ -14,9 +14,7 @@ const UserManagementPage = () => {
   const [isFormVisible, setIsFormVisible] = useState(false);
   const [users, setUsers] = useState([]);
   const [isUserListVisible, setIsUserListVisible] = useState(true);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const [isPasswordPopupOpen, setIsPasswordPopupOpen] = useState(false);
+  const [activePopup, setActivePopup] = useState(null);
 
   // Fetch users when the component mounts
   useEffect(() => {
@@ -24,17 +22,10 @@ const UserManagementPage = () => {
       .get("api/caseworker/user/all")
       .then((response) => {
         if (response.status === 200) {
-          const fetchedUsers = response?.data;
-          setUsers(fetchedUsers);
-        } else {
-          console.error(
-            `Error while fetching overview data. Status: ${response.status}`
-          );
-          alert("Fel vid laddning av användare. Försök igen senare.");
-        }
+          setUsers(response.data);
+        } else alert("Fel vid laddning av användare. Försök igen senare.");
       })
-      .catch((error) => {
-        console.log("API Error:", error);
+      .catch(() => {
         alert("Fel uppstod vid hämtning av användare.");
       });
   }, []);
@@ -63,33 +54,20 @@ const UserManagementPage = () => {
   // Handle form submission
   const handleFormSubmit = async (formData) => {
     try {
-      if (selectedUser) {
-        if (!formData.id) {
-          console.error("Error updating user: ID is missing");
-          alert("Användare utan ID");
-          return;
-        }
-        // Update user
-        const url = `api/caseworker/user/${formData.id}`;
-        console.log("updating user at:", url);
-        const response = await axios.put(url, formData);
-
-        if (response.status === 200) {
-          const updatedUser = response.data;
-          setUsers((prevUsers) =>
-            prevUsers.map((user) =>
-              user.id === updatedUser.id ? updatedUser : user
-            )
-          );
-          alert("Användare uppdaterad framgångsrikt!");
-        }
+      const url = `api/caseworker/user/${formData.id}`;
+      const response = await axios.put(url, formData);
+      if (response.status === 200) {
+        const updatedUser = response.data;
+        setUsers((prevUsers) =>
+          prevUsers.map((user) =>
+            user.id === updatedUser.id ? updatedUser : user
+          )
+        );
+        alert("Användare uppdaterad framgångsrikt!");
       } else {
-        // Create new user
-        console.log("Creating new user");
-        const response = await axios.post("api/caseworker/user", formData);
-
-        if (response.status === 200) {
-          const newUser = response.data;
+        const respons = await axios.post("api/caseworker/user", formData);
+        if (respons.status === 200) {
+          const newUser = respons.data;
           setUsers((prevUsers) => [...prevUsers, newUser]);
           alert("Användare skapad framgångsrikt!");
         }
@@ -97,103 +75,84 @@ const UserManagementPage = () => {
       closeForm();
     } catch (error) {
       console.error("Error updating user:", error);
-      alert("Fel vid uppdatering av användare.");
     }
   };
-
   // Handle deleting a user
   const handleDeleteUser = async () => {
-    if (!selectedUser || !selectedUser.id) {
-      console.error("Error deleting user: ID is missing");
-      alert("Användare utan ID");
-      return;
-    }
-    setIsDeleting(true);
-    try {
-      const response = await axios.delete(
-        `api/caseworker/user/${selectedUser.id}`
-      );
-      if (response.status === 200) {
-        setUsers((prevUsers) =>
-          prevUsers.filter((u) => u.id !== selectedUser.id)
-        );
-        alert("Användare raderad framgångsrikt!");
-        closeForm();
-      } else {
-        alert("Användaren kunde inte hittas.");
-      }
-    } catch (error) {
-      console.error("Error deleting user:", error);
-      alert("Fel vid radering av användare.");
-    } finally {
-      setIsDeleting(false);
-      setIsPopupOpen(false);
-    }
+    if (!selectedUser?.id) return alert("Användare utan ID");
+    await axios.delete(`api/caseworker/user/${selectedUser.id}`);
+    setUsers((prevUsers) => prevUsers.filter((u) => u.id !== selectedUser.id));
+    alert("Användare raderad framgångsrikt!");
+    closePopup();
+    closeForm();
   };
-
   // Handle password reset
   const handlePasswordUpdate = async (newPassword) => {
-    const userId = selectedUser?.id;
-    if (!userId) {
-      alert("Användare utan ID");
-      return;
-    }
-
-    try {
-      const response = await axios.put(`api/caseworker/user/${userId}`, {
+    if (!selectedUser?.id) return alert("Användare utan ID");
+    const updatedUser = await axios.put(
+      `api/caseworker/user/${selectedUser.id}`,
+      {
         password: newPassword,
-      });
-
-      if (response.status === 200) {
-        const updatedUser = response.data;
-
-        const updatedUserFormLocal = updateUser({
-          ...updatedUser,
-          password: newPassword,
-        });
-
-        setUsers((prevUsers) =>
-          prevUsers.map((user) =>
-            user.id === updatedUserFormLocal.id ? updatedUserFormLocal : user
-          )
-        );
-
-        alert("Lösenord uppdaterat framgångsrikt!");
-        setIsPasswordPopupOpen(false);
-      } else {
-        alert("Användaren kunde inte hittas.");
       }
-    } catch (error) {
-      console.error("Error updating password:", error);
-      alert("Fel vid uppdatering av lösenord.");
-    }
+    );
+    setUsers((prevUsers) =>
+      prevUsers.map((user) =>
+        user.id === updatedUser.data.id ? updatedUser.data : user
+      )
+    );
+
+    alert("Lösenord uppdaterat framgångsrikt!");
+    closePopup();
   };
 
+  /* // Handle Update a user
+  const handleUpdatedUser = async (formData) => {
+    const respons = await axios.post(
+      `api/caseworker/user${formData.id}`,
+      formData
+    );
+    const updatedUser = respons.data;
+    setUsers((prevUsers) =>
+      prevUsers.map((user) => (user.id === updatedUser.id ? updatedUser : user))
+    );
+    alert("Användare uppdaterad framgångsrikt!");
+  };
+
+  // Handle creating a new user
+  const handleCreateUser = async (formData) => {
+    const response = await axios.post("api/caseworker/user", formData);
+    setUsers((prevUsers) => [...prevUsers, newUser]);
+    alert("Användare skapad framgångsrikt!");
+  }; */
+
   // Handle Popup Open
-  const openPasswordPopup = () => {
-    setIsPasswordPopupOpen(true);
+  const openPopup = (popupType) => {
+    setActivePopup(popupType);
   };
 
   // Handle Popup Close
-  const closePasswordPopup = () => {
-    setIsPasswordPopupOpen(false);
+  const closePopup = () => {
+    setActivePopup(null);
+  };
+
+  const handleOpenDeletePopup = () => {
+    openPopup("delete");
+  };
+
+  const handleOpenPasswordPopup = () => {
+    openPopup("password");
   };
 
   return (
     <div className="container mx-auto p-4">
       <div className="flex justify-between items-center border-b-2 border-gray-200 mb-6 pb-3">
-        {(isUserListVisible && (
-          <h1 className="text-2xl font-bold">Hantera användare</h1>
-        )) ||
-          (isFormVisible && !selectedUser && (
-            <h1 className="text-2xl font-bold">Skapa konto för användare</h1>
-          )) ||
-          (isFormVisible && selectedUser && (
-            <h1 className="text-2xl font-bold">
-              Redigera Profil - {selectedUser?.first_name}{" "}
-              {selectedUser?.last_name}
-            </h1>
-          ))}
+        <h1 className="text-2xl font-bold">
+          {isUserListVisible
+            ? "Hantera användare"
+            : selectedUser
+            ? `Redigera Profil - ${selectedUser.first_name} ${selectedUser.last_name}`
+            : "Skapa konto för användare"}
+        </h1>
         {!isFormVisible && (
           <button
             onClick={openCreateForm}
@@ -216,29 +175,27 @@ const UserManagementPage = () => {
         <UserForm
           isEditing={!!selectedUser}
           user={selectedUser}
-          isDeleting={isDeleting}
           onSubmit={handleFormSubmit}
-          onDelete={() => setIsPopupOpen(true)}
-          onPasswordUpdate={openPasswordPopup}
+          onDelete={handleOpenDeletePopup}
+          onPasswordUpdate={handleOpenPasswordPopup}
           onClose={closeForm}
         />
       )}
       {/* Delete Confirmation Popup */}
-      <DeleteConfirmationPopup
-        isOpen={isPopupOpen}
-        user={selectedUser}
-        onClose={() => setIsPopupOpen(false)}
-        onConfirm={handleDeleteUser}
-      />
+      {activePopup === "delete" && (
+        <DeleteConfirmationPopup
+          isOpen={activePopup === "delete"}
+          user={selectedUser}
+          onClose={closePopup}
+          onConfirm={handleDeleteUser}
+        />
+      )}
       {/* Password Confirmation Popup */}
-      {isPasswordPopupOpen && (
+      {activePopup === "password" && (
         <PasswordConfirmationPopup
-          isOpen={isPasswordPopupOpen}
-          onClose={closePasswordPopup}
-          onConfirm={(newPassword) => {
-            console.log("Confirming password update");
-            handlePasswordUpdate(newPassword);
-          }}
+          isOpen={activePopup === "password"}
+          onClose={closePopup}
+          onConfirm={handlePasswordUpdate}
         />
       )}
     </div>
