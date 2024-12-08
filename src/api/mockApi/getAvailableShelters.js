31ef32372c053bffb288f120aada20fa214a0650
Abubaker <Abubakersalah4@gmail.com>
2024-12-08 20:09:10 +0100

    Add to AccommodationAmenitiesDisplay.jsx and getAvailableShelters.js

--- src/api/mockApi/getAvailableShelters.js
+++ src/api/mockApi/getAvailableShelters.js
@@ -57,58 +57,38 @@ export function getAvailableShelters() {
                     name: "Rum",
                     description: "Description Rum 1",
                     total_places: 5,
-                    host: { 
-                        region: {
-                            id: 1,
-                            name: "Stockgolm"
-                        },
-                        id: 28,
-                        name: "4 bäddsrum",
-                        street: "Storvägen 12",
-                        postcode: "12345",
-                        city: "Teststad"
-                    },
+                    places_left: 3,
                     type: "woman-only",
-                    places_left: 3
+                    
+                    
                 },
                 {
                     id: 50,
                     name: "Hygienartiklar",
                     description: "Tvål och shampoo",
                     total_places: 0,
-                    host: { 
-                        region: {
-                            id: 1,
-                            name: "Test"
-                        },
-                        id: 28,
-                        name: "Test",
-                        street: "Test Street 12",
-                        postcode: "12345",
-                        city: "Teststad"
-                    },
+                    places_left: 0,
                     type: "hygieneproducts",
-                    places_left: 0
+                    features: [
+                        { label: "Öppet", value: "Dygnet runt."},
+                        { label: "Egen toalett", value: "Nej" },
+                        { label: "Egen dusch", value: "Nej" },
+                        { label: "Hiss", value: "Nej" },
+                        { label: "TV", value: "Ja, i gemensamma utrymmen" },
+                        { label: "Wi-Fi", value: "Ja" },
+                        { label: "Bara kvinnor", value: "Ja" },
+                        { label: "Tillgänglighetsanpassning för rörelsehindrade", value: "Ja" },
+                    ],
                 },
                 {
                     id: 52,
                     name: "Rum",
                     description: "Description Rum 2",
                     total_places: 4,
-                    host: { 
-                        region: {
-                            id: 1,
-                            name: "Test"
-                        },
-                        id: 28,
-                        name: "Test",
-                        street: "Test Street 12",
-                        postcode: "12345",
-                        city: "Teststad"
-                    },
+                    places_left: 1,
                     type: "room",
-                    places_left: 1
-                },              
+                    
+                },
             ]
         },
         {
@@ -127,39 +107,85 @@ export function getAvailableShelters() {
                 {
                     id: 2,
                     name: "4-bädsrum ",
-                    description: " Rum 2",
+                    description: "Rum 2",
                     total_places: 4,
-                    host: { 
-                        region: {
-                            id: 1,
-                            name: "Test"
-                        },
-                        id: 1,
-                        name: "Test",
-                        street: "Test Street 12",
-                        postcode: "12345",
-                        city: "Teststad"
-                    },
+                    places_left: 2,
                     type: "room",
+                    features: [
+                        { label: "Öppet", value: "Dygnet runt."},
+                        { label: "Matsal", value: "Ja" },
+                        { label: "Wi-Fi", value: "Ja" },
+                        { label: "Hiss", value: "Nej" },
+                        { label: "Rökning inomhus", value: "Nej" },
+                        { label: "Husdjur", value: "Nej" },
+                        
+                    ],
                 },
                 {
                     id: 3,
                     name: "Sovsal",
                     description: "Description Rum 2",
                     total_places: 15,
-                    host: { 
-                        region: {
-                            id: 1,
-                            name: "Test"
-                        },
-                        id: 1,
-                        name: "Test",
-                        street: "Test Street 12",
-                        postcode: "12345",
-                        city: "Teststad"
-                    },
+                    places_left: 5,
                     type: "room",
-                }            
+                    features: [
+
+                        { label: "Egen toalett", value: "Nej" },
+                        { label: "Egen dusch", value: "Nej" },
+                        { label: "Pentry", value: "Nej" },
+                        { label: "TV", value: "Ja" },
+                        { label: "Tillgänglighetsanpassning för rörelsehindrade", value: "Nej" },
+                    ],
+                },
+               
+            ]
+        },
+        {
+            host: { 
+                region: {
+                    id: 5,
+                    name: "Stockholm"
+                },
+                id: 5,
+                name: "Hvilan",
+                street: "Ingemarsgatan 9",
+                postcode: "113 54",
+                city: "Stockholm"
+            },
+            products: [
+                {
+                    id: 5,
+                    name: "Dubbelrum",
+                    description: "Rum 2",
+                    total_places: 13,
+                    places_left: 5,
+                    type: "room",
+                    features: [
+                        { label: "Öppet", value: "Dygnet runt."},
+                        { label: "Matsal", value: "Ja" },
+                        { label: "Wi-Fi", value: "Ja" },
+                        { label: "Hiss", value: "Nej" },
+                        { label: "Rökning inomhus", value: "Nej" },
+                        { label: "Husdjur", value: "Nej" },
+                        
+                    ],
+                },
+                {
+                    id: 6,
+                    name: "Enkelrum",
+                    description: "Description Rum 2",
+                    total_places: 8,
+                    places_left: 4,
+                    type: "room",
+                    features: [
+
+                        { label: "Egen toalett", value: "Nej" },
+                        { label: "Egen dusch", value: "Nej" },
+                        { label: "Pentry", value: "Nej" },
+                        { label: "TV", value: "Ja" },
+                        { label: "Tillgänglighetsanpassning för rörelsehindrade", value: "Nej" },
+                    ],
+                },
             ]
         }
     ];
--- src/components/User/AccommodationAmenitiesDisplay.jsx
+++ src/components/User/AccommodationAmenitiesDisplay.jsx
@@ -1,31 +1,50 @@
-import AccomodationDetailProductIcon from "./AccomodationDetailProductIcon"
+import AccomodationDetailProductIcon from "./AccomodationDetailProductIcon";
 import PropTypes from "prop-types";
 
-export default function AccommodationAmenitiesDisplay({title, products}) {
+export default function AccommodationAmenitiesDisplay({ title, products }) {
   AccommodationAmenitiesDisplay.propTypes = {
     title: PropTypes.string.isRequired,
     products: PropTypes.array.isRequired,
   };
 
   const productCount = products.length > 0 ? products.length : null;
-  
-  return(
+
+  return (
     <>
-    {productCount ? (
-      <>
-        <p className="text-base font-bold grid justify-items-start "> {title}</p>
-        <div className={`grid md:grid-cols-5 gap-2 md:gap-5`}>
-          {products.map((product) => (
-            <div key={product.id} className="flex gap-2">
-              <AccomodationDetailProductIcon type={product.type} />
-              <p className="accommodation_amenities_display_paragraph">{product.name}</p>
-            </div>
-          ))}
-        </div>  
-      </>
-    ) : (
-      <p/>
-    )}
+      {productCount ? (
+        <>
+          <p className="text-base font-bold grid justify-items-start">
+            {title}
+          </p>
+          <div className={`grid md:grid-cols-5 gap-2 md:gap-5`}>
+            {products.map((product) => (
+              <div
+                key={product.id}
+                className="flex flex-col justify-start items-start gap-6"
+              >
+                <div className="flex flex-col">
+                  <AccomodationDetailProductIcon type={product.type} />
+                  <p className="accommodation_amenities_display_paragraph">
+                    {product.name}
+                  </p>
+                </div>
+                {product.features && product.features.length > 0 && (
+                  <ul className="list-disc ml-3">
+                    {product.features.map((feature, index) => (
+                      <li key={index} className="pb-5">
+                        <span className="font-bold mr-2">{feature.label}:</span>{" "}
+                        {feature.value}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <p />
+      )}
     </>
-  )
+  );
 }
