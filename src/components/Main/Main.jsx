b29b6693edc589521847a97daded94e49abc635c
Selsabeel A. <90451756+SelsabeelA@users.noreply.github.com>
2024-12-10 18:02:49 +0200

    Move botpress to main instead of navbar

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,6 +6,24 @@ export default function Main({ children }) {
     children: PropTypes.node.isRequired,
   };
 
+    // Inject Botpress scripts
+    useEffect(() => {
+      const botpressScript1 = document.createElement("script");
+      botpressScript1.src = "https://cdn.botpress.cloud/webchat/v2.2/inject.js";
+      botpressScript1.async = true;
+  
+      const botpressScript2 = document.createElement("script");
+      botpressScript2.src = "https://files.bpcontent.cloud/2024/11/02/09/20241102093854-JYPQTPG9.js";
+      botpressScript2.async = true;
+  
+      document.body.appendChild(botpressScript1);
+      document.body.appendChild(botpressScript2);
+  
+      return () => {
+        document.body.removeChild(botpressScript1);
+        document.body.removeChild(botpressScript2);
+      };
+    }, []);
   return (
     /* Main, Här ska allt skit som dyka upp dyka upp */
     <div className="flex justify-center bg-background-white">{children}</div>
