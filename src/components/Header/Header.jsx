2aafd19a3fb1b8f167c9697c7c40d5289a00fac5
rohan-20 <20rohan.sharma08@gmail.com>
2025-02-13 23:14:31 +0100

    Deleted upper menu for Guest

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,17 +6,22 @@ import Iconbar from "./Iconbar";
 
 export default function Header() {
   const { login } = useLogin();
+  const viewerGroup =
+    login?.usergroups instanceof Array ? login?.usergroups[0] : null;
 
   return login?.first_name || login?.last_name ? (
     <div>
-      <Navbar
-        first_name={
-          Array.isArray(login.first_name)
-            ? login.first_name[0]
-            : login.first_name
-        }
-        last_name={login.last_name}
-      />
+      {viewerGroup !== "user" ? (
+        <Navbar
+          first_name={
+            Array.isArray(login.first_name)
+              ? login.first_name[0]
+              : login.first_name
+          }
+          last_name={login.last_name}
+        />
+      ) : null}
+      
     </div>
   ) : (
     <div>
