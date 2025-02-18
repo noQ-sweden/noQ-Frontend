5d2bea8b1f7241c1504736cdd62f0d7b58e0266b
Tobias Johansson <tobias@noq.nu>
2025-02-18 10:44:09 +0100

    refactoring...

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,19 +1,13 @@
-import { useState } from "react";
 import axios from "./../api/AxiosNoqApi";
 
 const useLogout = () => {
-  const [isLoggingOut, setIsLoggingOut] = useState(false);
-
   const logout = async () => {
     try {
-      setIsLoggingOut(true);
       // Send a request to the backend server to log out the user
       await axios.get("/api/logout/", { withCredentials: true });
     } catch (error) {
       // maybe some better error handling here
       console.log("An error occurred while logging out. Please try again.");
-    } finally {
-      setIsLoggingOut(false); // reset the state
     }
   };
 
