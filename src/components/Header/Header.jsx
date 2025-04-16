fe9257292d45a5e5274f2b2fc232afe7074a8251
rohan-noq <rohan@noq.nu>
2025-04-16 11:21:45 +0200

    Error fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,9 +13,9 @@ export default function Header() {
   return login?.first_name || login?.last_name ? (
     <div>
 
-      <div className="bg-[#245b56] p-8 h-10 flex items-center w-full fixed top-0 left-0 z-10">
+      {/* <div className="bg-[#245b56] p-8 h-10 flex items-center w-full fixed top-0 left-0 z-10">
         <img src="src/assets/images/NoqIconWhiteOnGreen.svg" alt="noQ" className="w-24" />
-      </div>
+      </div> */}
 
       {viewerGroup !== "user" ? (
         <Navbar
