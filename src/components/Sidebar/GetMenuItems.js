30416e1976697d75d2d4b59cfd250a31095048b9
Jani Rutanen <jani@noq.nu>
2025-05-05 12:50:57 +0200

    Update GetMenuItems.js
    Adds menu for administering volunteers and comments out unused menu items

--- src/components/Sidebar/GetMenuItems.js
+++ src/components/Sidebar/GetMenuItems.js
@@ -1,6 +1,6 @@
 import {
     FaChartPie,
-    FaReceipt,
+    //FaReceipt,
     FaBell,
     FaUser,
     FaRandom,
@@ -19,9 +19,10 @@ export default function GetMenuItems(userGroup) {
         { icon: FaBell, label: "Förfrågningar", sideBarLink: "host/requests" },
         { icon: FaCalendarAlt, label: "Kalender" },
         { icon: FaRandom, label: "Mina Rum", sideBarLink: "host/products" },
-        { icon: FaRandom, label: "Härberget" },
-        { icon: FaUser, label: "Gäster" },
-        { icon: FaReceipt, label: "Fakturering" },
+        { icon: FaUser, label: "Volontärhantering", sideBarLink: "host/volunteers" },
+        //{ icon: FaRandom, label: "Härberget" },
+        //{ icon: FaUser, label: "Gäster" },
+        //{ icon: FaReceipt, label: "Fakturering" },
     ];
 
     const caseworkerSidebarItemsTop = [
