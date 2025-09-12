8e93a536ac9da9ef5f93536e0317195bdb505094
Mario Fernández <nilovinilo1@gmail.com>
2025-09-12 11:22:40 +0200

    feat(sidebar): switch to text-only, i18-driven meny and mobile-first layout - Add i18n init + locales (sv/en); wire translation keys for sidebar - Replace hard-coded labels with t(labelKey) - Clean unused mock/user conditions; minor Tailwind config touch-ups

--- src/i18n.js
+++ src/i18n.js
@@ -1,32 +1,37 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import enTranslation from './locales/en.json'
-import svTranslation from './locales/sv.json'
-import plTranslation from './locales/pl.json'
-import roTranslation from './locales/ro.json'
+import en from "./locales/en.json";
+import sv from "./locales/sv.json";
+import pl from "./locales/pl.json";
+import ro from "./locales/ro.json";
 
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
-const resources = {
+/* const resources = {
     sv: { translation: svTranslation },
     en: { translation: enTranslation },
     pl: { translation: plTranslation },
     ro: { translation: roTranslation }
-}
+} */
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    resources,
+    resources: {
+      sv: { translation: sv },
+      en: { translation: en },
+      pl: { translation: pl },
+      ro: { translation: ro },
+    },
     lng: "sv", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
-
+    fallbackLng: "en",
     interpolation: {
-      escapeValue: false // react already safes from xss
-    }
+      escapeValue: false, // react already safes from xss
+    },
   });
 
-  export default i18n;
+export default i18n;
--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginProvider } from "./context/LoginProvider";
 import { HostProvider } from "./context/HostProvider";
 import { AccommodationProvider } from "./context/AccommodationProvider";
 import { HeaderProvider } from "./context/HeaderProvider";
 import App from "./App.jsx";
 import "./index.css";
-import "./i18n.js"
-import { CompassProvider } from "./context/CompassContext"; 
-
-
+import "./i18n";
+import { CompassProvider } from "./context/CompassContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <CompassProvider>
-            <BrowserRouter>
-                <LoginProvider>
-                    <HeaderProvider>
-                        <HostProvider>
-                            <AccommodationProvider>
-                                <Routes>
-                                    <Route path="/*" element={<App />} />
-                                </Routes>
-                            </AccommodationProvider>
-                        </HostProvider>
-                    </HeaderProvider>
-                </LoginProvider>
-            </BrowserRouter>
-        </CompassProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <CompassProvider>
+      <BrowserRouter>
+        <LoginProvider>
+          <HeaderProvider>
+            <HostProvider>
+              <AccommodationProvider>
+                <Routes>
+                  <Route path="/*" element={<App />} />
+                </Routes>
+              </AccommodationProvider>
+            </HostProvider>
+          </HeaderProvider>
+        </LoginProvider>
+      </BrowserRouter>
+    </CompassProvider>
+  </React.StrictMode>
 );
